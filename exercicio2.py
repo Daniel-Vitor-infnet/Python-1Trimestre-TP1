e6ebb9b00b1a2008f82695e0da5c04f8f47e6305
Daniel Vitor Madeira Reis <147751321+Daniel-Vitor-infnet@users.noreply.github.com>
2025-08-08 18:37:29 -0300

    Create exercicio2.py

---
+++ exercicio2.py
@@ -0,0 +1,10 @@
+nome_cliente: str= 'Daniel Vitor Madeira Reis'
+idade_cliente: int = 23
+score_credito: float = 772.4
+cliente_ativo: bool  = True
+
+
+print(f'Nome: {nome_cliente} | Tipo: {type(nome_cliente)}')
+print(f'Idade: {idade_cliente} | Tipo: {type(idade_cliente)}')
+print(f'Score de Crédito: {score_credito} | Tipo: {type(score_credito)}')
+print(f'Cliente Ativo: {cliente_ativo} | Tipo: {type(cliente_ativo)}')
