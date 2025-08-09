ec00aa7c6305fc16a45d0d2d14dbcbc2cb68a1fb
Daniel Vitor Madeira Reis <147751321+Daniel-Vitor-infnet@users.noreply.github.com>
2025-08-09 12:56:22 -0300

    Create exercicio3.py

---
+++ exercicio3.py
@@ -0,0 +1,13 @@
+#Fiquei bem confuso com esse "placeholders" de idade em um problema de km, porém vou tentar fazer meu melhor.
+
+km_por_dia: int = 23 # Pensei em usar float, porém não vejo necessidade.
+gasto_diario: int = 300 + 2 # Sou de 2002 e o enunciado pede 2 digitos, porém n vejo sentido em colocar 02 como é uma soma 02 é igual a 2.
+
+
+
+print(f'Total percorrido em uma semana de {km_por_dia * 7}km')
+print(f'Diferença entre 100 reais e o gasto diário é de {gasto_diario - 100} reais')
+print(f'Quantos dias o valor de R$500 cobre {500 // gasto_diario} dias')
+print(f'Porcentagem do gasto diário em relação a 100 é de {gasto_diario % 100}')
+print(f'Média diária de custo por km rodado é de {gasto_diario / km_por_dia} reais por km')
+
