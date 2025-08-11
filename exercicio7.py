5d132785d655af0d00830e5df7b2d37b0e84028e
Daniel Vitor Madeira Reis <147751321+Daniel-Vitor-infnet@users.noreply.github.com>
2025-08-11 18:00:46 -0300

    Create exercicio7.py

---
+++ exercicio7.py
@@ -0,0 +1,5 @@
+# Fiquei bem confuso pq quando parece "simples" é normalmente uma pegadinha. Porem usei o "\n" para melhorar a legibilidade.
+
+msg_alerta: str = "ATENÇÃO! Essa é uma mensagem de alerta.\n"
+
+print(msg_alerta * 5)
