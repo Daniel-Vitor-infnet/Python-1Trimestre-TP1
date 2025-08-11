b55bddacdc79f537499b4141c246303ab3dfed80
Daniel Vitor Madeira Reis <147751321+Daniel-Vitor-infnet@users.noreply.github.com>
2025-08-11 20:22:18 -0300

    Create exercicio11.py

---
+++ exercicio11.py
@@ -0,0 +1,6 @@
+# Eu n quis usar o "dadosPessoais" nesse exercicio, pois por ser focado em conversão de txt para int.
+
+ano_txt: str = "2002"
+ano_int: int = int(ano_txt)
+
+print(f"Meu ano de nascimento mais 5 é igual a {ano_int + 5}.")
